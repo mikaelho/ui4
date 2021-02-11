dd5884695bcc0e6fe0f2fee228eedc4e9bfcc8e4
Mikael <mikael.honkala@gmail.com>
2021-02-12 01:21:26 +0200

    JS PoC

--- ui4/dom/ui4.js
+++ ui4/dom/ui4.js
@@ -1,46 +1,100 @@
-ui4 = {
-  attrs: {
-    top: "leading",
-    left: "leading",
+(function (ui4, undefined) {
+  
+  var LEADING="leading", TRAILING="trailing", NEUTRAL="neutral";
+  
+  var attrTypes = {
+    width: NEUTRAL,
+    left: LEADING
+  };
+  
+  var allDependencies = {};
+  
+  function checkDependecies() {
+    for (const [targetId, dependencies] of Object.entries(allDependencies)) {
+      dependencies.forEach( (d) => {
+        console.log(JSON.stringify(d));
+        var sourceElem = document.getElementById(d.sourceId);
+        var targetElem = document.getElementById(targetId);
+        var sourceStyle = window.getComputedStyle(sourceElem);
+        var targetStyle = window.getComputedStyle(targetElem);
+        d.updateFunc(sourceStyle, d.sourceAttr, targetElem, targetStyle, d.targetAttr);
+      });
+    }
+  }
+  
+  function updateEqual(sourceStyle, sourceAttr, targetElem, targetStyle, targetAttr) {
+    var sourceValue = sourceStyle.getPropertyValue(sourceAttr);
+    var targetValue = targetStyle.getPropertyValue(targetAttr);
+    if (sourceValue !== targetValue) {
+      targetElem.style[targetAttr] = sourceValue;
+    }
+  }
+  
+  function getUpdateFunction(source, target) {
+    if (attrTypes[source] === attrTypes[target]) {
+      return updateEqual;
+    }
+  }
+  
+  function setDependencies(node) {
+    var targetId = node.id; 
+    if (!targetId) { return; }
     
-  },
-  classChangeHandler: function (mutations, observer) {
+    var ui4AttrValue = node.getAttribute("ui4");
+    if (!ui4AttrValue) { return; }
+
+    var dependencies = Array();
+    var specs = ui4AttrValue.split(" ");
+    specs.forEach( (spec) => {
+      let tokens = spec.split(/\W/);
+      dependencies.push({
+        updateFunc: getUpdateFunction(tokens[2], tokens[0]),
+        targetAttr: tokens[0],
+        sourceId: tokens[1],
+        sourceAttr: tokens[2]
+      });
+    });
+    if (!dependencies.length) {
+      delete allDependencies[targetId];
+    } else {
+      allDependencies[targetId] = dependencies;
+    }
+  }
+  
+  function classChangeHandler(mutations, observer) {
     mutations.forEach( (mutation) => {
       switch(mutation.type) {
         case 'childList':
           mutation.addedNodes.forEach( (node) => {
-            console.log(node.className)
+            setDependencies(node);
           });
-          console.log(mutation.removedNodes)
+          //console.log(mutation.removedNodes)
           break;
         case 'attributes':
-          console.log(mutation.attributeName);
+        setDependencies(mutation.target);
           break;
       }
     });
-  },
-  startClassObserver: function () {
-    const observer = new MutationObserver(ui4.classChangeHandler);
+  }
+  
+  ui4.startClassObserver = function () {
+    const observer = new MutationObserver(classChangeHandler);
     observer.observe(document, {
       subtree: true,
       childList: true,
-      attributeFilter: ["class"]
+      attributeFilter: ["ui4"]
     });
-  },
-  startTracking: function (sourceId, sourceAttr, targetId, targetAttr) {
-    const sourceElem = document.getElementById(sourceId);
-    const targetElem = document.getElementById(targetId);
-    const observer = new MutationObserver(
-      function () {
-        console.log("hep");
-        var sourceStyle = window.getComputedStyle(sourceElem);
-        targetElem.style[targetAttr] = sourceStyle.getPropertyValue(sourceAttr);
-      }
-    );
+  };
+  
+  ui4.startTracking = function () {
+    const observer = new MutationObserver(checkDependecies);
     observer.observe(document.body, {
       subtree: true,
       childList: true,
       attributeFilter: ["style"]
     });
   }
-}
+  
+} ( window.ui4 = window.ui4 || {} ));
+
+ui4.startClassObserver();
