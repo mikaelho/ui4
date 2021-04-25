31cc74a1f10811bcd1259798c3275de85ffa3542
Mikael <mikael.honkala@gmail.com>
2021-04-25 13:01:53 +0300

    List dependency test

--- test/test_ui4.js
+++ test/test_ui4.js
@@ -17,7 +17,7 @@ const test = window.ui4._privateForTesting;
 assert = require('assert');
 
 describe("#parseSpec", () => {
-  it('should parse spec from backend', async () => {
+  it('should parse 1-dependency spec from backend', async () => {
     const spec = {
       a0: "left",
       a5: 200
@@ -34,41 +34,50 @@ describe("#parseSpec", () => {
     
     assert.deepEqual(result, expected);
   });
+  
+  it('should parse list dependency spec with animation', async () => {
+    const spec = {
+      key: "max",
+      list: [
+        {
+          a0: "left",
+          a5: 200
+        },
+        {
+          a0: "left",
+          a5: 300
+        }
+      ],
+      a6: 0.3
+    };
+    
+    const result = test.parseSpec(spec, "id22");
+    
+    const expected = {
+      key: "max",
+      list: [
+        {
+          targetId: 'id22',
+          targetAttr: 'left',
+          comparison: '=',
+          modifier: 200
+        },
+        {
+          targetId: 'id22',
+          targetAttr: 'left',
+          comparison: '=',
+          modifier: 300
+        }
+      ],
+      duration: 0.3
+    };
+    
+    assert.deepEqual(result, expected);
+  });
 });
 
 describe("#toCamelCase", () => {
   it('should turn CSS style name to JS variable name', async () => {
     assert.equal(test.toCamelCase('background-color'), 'backgroundColor');
-  });
-});
-
-describe("#maxMinCheck", () => {
-  it('should retain and return true for the maximum/minimum values', async () => {
-    const maxValues = {left: 100};
-    const minValues = {};
-    const dependency = {require: 'max', targetAttr: 'left'};
-
-    shouldApply = test.maxMinCheck(dependency, 200, maxValues, minValues);
-    assert.equal(shouldApply, true);
-    assert.equal(maxValues.left, 200);
-
-    shouldApply = test.maxMinCheck(dependency, 100, maxValues, minValues);
-    assert.equal(shouldApply, false);
-    assert.equal(maxValues.left, 200);
-
-    delete dependency.require;
-    shouldApply = test.maxMinCheck(dependency, 100, maxValues, minValues);
-    assert.equal(shouldApply, true);
-    assert.equal(maxValues.left, 200);
-
-    dependency.require = 'min';
-    dependency.targetAttr = 'bottom';
-    shouldApply = test.maxMinCheck(dependency, 150, maxValues, minValues);
-    assert.equal(shouldApply, true);
-    assert.equal(minValues.bottom, 150);
-
-    shouldApply = test.maxMinCheck(dependency, -150, maxValues, minValues);
-    assert.equal(shouldApply, true);
-    assert.equal(minValues.bottom, -150);
   });
 });
